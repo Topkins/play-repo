f2af123c8a1b5b1fdacf683d9465f87a435cc694
Tom Hopkins <Tom Hopkins>
2017-04-07 19:24:06 +0100

    created maven project

---
+++ my-play-repo/src/main/java/com/play-repo/app/App.java
@@ -0,0 +1,13 @@
+package com.play-repo.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
